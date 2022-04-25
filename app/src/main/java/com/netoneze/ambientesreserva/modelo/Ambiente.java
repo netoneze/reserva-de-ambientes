0fe30302865b6e4d6c51657c3d2a4ce6b9e49a82
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-04-24 21:12:08 -0300

    feat: Added necessary attributes.

--- app/src/main/java/com/netoneze/ambientesreserva/modelo/Ambiente.java
+++ app/src/main/java/com/netoneze/ambientesreserva/modelo/Ambiente.java
@@ -2,28 +2,57 @@ package com.netoneze.ambientesreserva.modelo;
 
 public class Ambiente {
 
-    private String nome;
-    private String tipo;
+    private String name;
+    private String type;
+    private String block;
+    private String department;
+    private String details;
 
-    public Ambiente (String nome, String tipo){
-        this.nome = nome;
-        this.tipo = tipo;
+    public Ambiente (String name, String type, String block, String department){
+        this.name = name;
+        this.type = type;
+        this.block = block;
+        this.department = department;
     }
 
-    public String getNome() {
-        return nome;
+    public String getDetails() {
+        return details;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setDetails(String details) {
+        this.details = details;
     }
 
-    public String getTipo() {
-        return tipo;
+    public String getBlock() {
+        return block;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
 }
