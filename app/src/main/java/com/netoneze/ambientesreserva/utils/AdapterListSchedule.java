e06afaec7b9aa2542c2f54a8e0f805a22ab86c2f
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-04-24 21:11:43 -0300

    feat: Added schedule listing adapter.

---
+++ app/src/main/java/com/netoneze/ambientesreserva/utils/AdapterListSchedule.java
@@ -0,0 +1,124 @@
+package com.netoneze.ambientesreserva.utils;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.netoneze.ambientesreserva.R;
+import com.netoneze.ambientesreserva.modelo.Ambiente;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdapterListSchedule extends BaseExpandableListAdapter {
+    private List<String> lstGrupos;
+    private HashMap<String, List<Ambiente>> lstItensGrupos;
+    private Context context;
+
+    public AdapterListSchedule(Context context, List<String> grupos, HashMap<String, List<Ambiente>> itensGrupos) {
+        // inicializa as variáveis da classe
+        this.context = context;
+        lstGrupos = grupos;
+        lstItensGrupos = itensGrupos;
+    }
+
+    @Override
+    public int getGroupCount() {
+        // retorna a quantidade de grupos
+        return lstGrupos.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        // retorna a quantidade de itens de um grupo
+        return lstItensGrupos.get(getGroup(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        // retorna um grupo
+        return lstGrupos.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        // retorna um item do grupo
+        return lstItensGrupos.get(getGroup(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        // retorna o id do grupo, porém como nesse exemplo
+        // o grupo não possui um id específico, o retorno
+        // será o próprio groupPosition
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        // retorna o id do item do grupo, porém como nesse exemplo
+        // o item do grupo não possui um id específico, o retorno
+        // será o próprio childPosition
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        // retorna se os ids são específicos (únicos para cada
+        // grupo ou item) ou relativos
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        // cria os itens principais (grupos)
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.grupo, null);
+        }
+
+        TextView tfTitulo = convertView.findViewById(R.id.tf_name);
+
+        Ambiente ambiente = (Ambiente) getChild(groupPosition, 0);
+
+        tfTitulo.setText((String) getGroup(groupPosition));
+
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        // cria os subitens (itens dos grupos)
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.item_grupo, null);
+        }
+
+        TextView tfBlock = convertView.findViewById(R.id.tf_block_body);
+        TextView tfType = convertView.findViewById(R.id.tf_type_body);
+        TextView tfDepartment = convertView.findViewById(R.id.tf_departament_body);
+        TextView tfDetails = convertView.findViewById(R.id.tf_details_body);
+
+        Ambiente ambiente = (Ambiente) getChild(groupPosition, childPosition);
+        tfBlock.setText(ambiente.getBlock());
+        tfType.setText(ambiente.getType());
+        tfDepartment.setText(ambiente.getDepartment());
+        tfDetails.setText(ambiente.getDetails());
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        // retorna se o subitem (item do grupo) é selecionável
+        return true;
+    }
+}
