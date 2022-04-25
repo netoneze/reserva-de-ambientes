50e2e42d21bd2c3eee2c45e548697451025f7142
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-04-24 21:12:48 -0300

    feat: Added bottom navigation listener for selected items.

--- app/src/main/java/com/netoneze/ambientesreserva/MainActivity.java
+++ app/src/main/java/com/netoneze/ambientesreserva/MainActivity.java
@@ -1,8 +1,11 @@
 package com.netoneze.ambientesreserva;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationBarView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener((NavigationBarView.OnItemSelectedListener) item -> {
+            switch (item.getItemId()) {
+                case R.id.home_page:
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.schedule_page:
+                    Toast.makeText(MainActivity.this, "Schedule", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.agenda_page:
+                    Toast.makeText(MainActivity.this, "Agenda", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.profile_page:
+                    Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        });
     }
 }
