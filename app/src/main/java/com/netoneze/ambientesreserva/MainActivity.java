5901f42bb194c8ee73fea0c4aeaaebf8c62584ff
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-05-15 18:04:33 -0300

    feat: Adjusted bottom navigation items call.

--- app/src/main/java/com/netoneze/ambientesreserva/MainActivity.java
+++ app/src/main/java/com/netoneze/ambientesreserva/MainActivity.java
@@ -1,40 +1,38 @@
 package com.netoneze.ambientesreserva;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     NavigationBarView bottomNavigationView;
-    HomeFragment homeFragment = new HomeFragment();
-    ScheduleFragment scheduleFragment = new ScheduleFragment();
+    ManagementFragment managementFragment = new ManagementFragment();
+    ReserveFragment reserveFragment = new ReserveFragment();
     AgendaFragment agendaFragment = new AgendaFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, homeFragment, "").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, reserveFragment, "").commit();
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
-                case R.id.home_page:
+                case R.id.reserve_page:
                     Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, homeFragment, "").commit();
-                    return true;
-                case R.id.schedule_page:
-                    Toast.makeText(MainActivity.this, "Schedule", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, scheduleFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, reserveFragment, "").commit();
                     return true;
                 case R.id.agenda_page:
                     Toast.makeText(MainActivity.this, "Agenda", Toast.LENGTH_SHORT).show();
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, agendaFragment).commit();
+                    return true;
+                case R.id.management_page:
+                    Toast.makeText(MainActivity.this, "Schedule", Toast.LENGTH_SHORT).show();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, managementFragment).commit();
                     return true;
             }
             return true;
