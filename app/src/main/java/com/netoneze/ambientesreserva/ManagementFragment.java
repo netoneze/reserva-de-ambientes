dd60e5a77acdbf707ea006454f778a1edc7817e0
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-05-15 18:00:41 -0300

    feat: Adjusted bottom navigation items.

--- app/src/main/java/com/netoneze/ambientesreserva/ManagementFragment.java
+++ app/src/main/java/com/netoneze/ambientesreserva/ManagementFragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleFragment#newInstance} factory method to
+ * Use the {@link ManagementFragment} factory method to
  * create an instance of this fragment.
  */
-public class ScheduleFragment extends Fragment {
+public class ManagementFragment extends Fragment {
 
-    public ScheduleFragment() {
+    public ManagementFragment() {
         // Required empty public constructor
     }
 
@@ -23,6 +23,6 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_management, container, false);
     }
 }
--- app/src/main/java/com/netoneze/ambientesreserva/ReserveFragment.java
+++ app/src/main/java/com/netoneze/ambientesreserva/ReserveFragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment} factory method to
+ * Use the {@link ReserveFragment} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class ReserveFragment extends Fragment {
 
-    public HomeFragment() {
+    public ReserveFragment() {
         // Required empty public constructor
     }
 
@@ -23,6 +23,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_reserve, container, false);
     }
 }
