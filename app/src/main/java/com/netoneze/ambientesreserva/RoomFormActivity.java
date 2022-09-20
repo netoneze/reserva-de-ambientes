e6dfbacd0fbec18e6aa4f9dd7e80d7a115c5a966
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-09-19 21:44:23 -0300

    feat: Added create room form view

---
+++ app/src/main/java/com/netoneze/ambientesreserva/RoomFormActivity.java
@@ -0,0 +1,14 @@
+package com.netoneze.ambientesreserva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_form);
+    }
+}
