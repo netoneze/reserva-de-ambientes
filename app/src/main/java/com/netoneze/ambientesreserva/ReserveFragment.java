06d8977c3ac403f6e20a5b48343ddcf85ec42306
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2022-10-12 17:18:01 -0300

    feat: Added verify for reservation in the same time and date for the same room.

--- app/src/main/java/com/netoneze/ambientesreserva/ReserveFragment.java
+++ app/src/main/java/com/netoneze/ambientesreserva/ReserveFragment.java
@@ -17,12 +17,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.netoneze.ambientesreserva.modelo.Reservation;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -39,7 +41,7 @@ public class ReserveFragment extends Fragment {
     Spinner spinnerRooms;
     Button buttonSave, buttonClean;
     EditText editTextDate, editTextStartTime, editTextEndTime, editTextPurpose;
-
+    Date myReservationDateStartTime, myReservationDateEndTime;
     public ReserveFragment() {
         // Required empty public constructor
     }
@@ -107,13 +109,46 @@ public class ReserveFragment extends Fragment {
             reserve.put("userId", user.getUid());
             reserve.put("purpose", editTextPurpose.getText().toString());
 
+            List<Reservation> lista = new ArrayList<>();
             db.collection("reservation")
-                    .add(reserve)
-                    .addOnSuccessListener(documentReference -> {
-                        Toast.makeText(getActivity(), "Saved reserve!", Toast.LENGTH_SHORT).show();
-                        cleanFields();
-                    })
-                    .addOnFailureListener(e -> Log.w("failure", "Error adding document", e));
+                    .whereEqualTo("room", spinnerRooms.getSelectedItem().toString())
+                    .whereEqualTo("date", editTextDate.getText().toString())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                HashMap<String, Map<String, Object>> documentMap = new HashMap<>();
+                                documentMap.put(document.getId(), document.getData());
+
+                                for (Map.Entry<String, Map<String, Object>> entry : documentMap.entrySet()) {
+                                    Reservation reservation = new Reservation();
+                                    Log.d("keyvalue", "Key = " + entry.getKey() + " Value = " + entry.getValue());
+                                    for (Map.Entry<String, Object> entryMap2 : entry.getValue().entrySet()) {
+                                        Log.d("keyvalue2", "Key = " + entryMap2.getKey() + " Value = " + entryMap2.getValue());
+                                        if (entryMap2.getKey().equals("room")) {
+                                            reservation.setRoom(entryMap2.getValue().toString());
+                                        }
+                                        if (entryMap2.getKey().equals("date")) {
+                                            reservation.setDate(entryMap2.getValue().toString());
+                                        }
+                                        if (entryMap2.getKey().equals("startTime")) {
+                                            reservation.setStartTime(entryMap2.getValue().toString());
+                                        }
+                                        if (entryMap2.getKey().equals("endTime")) {
+                                            reservation.setEndTime(entryMap2.getValue().toString());
+                                        }
+                                        if (entryMap2.getKey().equals("purpose")) {
+                                            reservation.setPurpose(entryMap2.getValue().toString());
+                                        }
+                                    }
+                                    lista.add(reservation);
+                                }
+                            }
+                            verifyReserveTime(lista, reserve);
+                        } else {
+                            Log.d("erro", "Error getting documents: ", task.getException());
+                        }
+                    });
         });
 
         buttonClean.setOnClickListener(v -> cleanFields());
@@ -128,6 +163,67 @@ public class ReserveFragment extends Fragment {
         editTextEndTime.setText("");
         editTextPurpose.setText("");
         spinnerRooms.setSelection(0);
+    }
+
+    public void verifyReserveTime(List<Reservation> lista, Map <String, Object> reserve) {
+        boolean saveReservation = true;
+        if (!lista.isEmpty()) {
+            SimpleDateFormat sdfTime = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US);
+            try {
+                myReservationDateStartTime = sdfTime.parse(editTextDate.getText().toString() +
+                        " " +
+                        editTextStartTime.getText().toString());
+                myReservationDateEndTime = sdfTime.parse(editTextDate.getText().toString() +
+                        " " +
+                        editTextEndTime.getText().toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for (Reservation reserveItem : lista) {
+                try {
+                    Date listReservationDateStartTime = sdfTime.parse(reserveItem.getDate() +
+                            " " +
+                            reserveItem.getStartTime());
+                    Date listReservationDateEndTime = sdfTime.parse(reserveItem.getDate() +
+                            " " +
+                            reserveItem.getEndTime());
+
+                    if ((myReservationDateStartTime.after(listReservationDateStartTime) || myReservationDateStartTime.compareTo(listReservationDateStartTime) == 0) &&
+                            (myReservationDateEndTime.before(listReservationDateEndTime) || myReservationDateEndTime.compareTo(listReservationDateEndTime) == 0)) {
+                        saveReservation = false;
+                    }
+
+                    if ((myReservationDateStartTime.after(listReservationDateStartTime) || myReservationDateStartTime.compareTo(listReservationDateStartTime) == 0) &&
+                            (myReservationDateStartTime.before(listReservationDateEndTime) || myReservationDateStartTime.compareTo(listReservationDateEndTime) == 0) &&
+                            myReservationDateEndTime.after(listReservationDateEndTime)) {
+                        saveReservation = false;
+                    }
+
+                    if ((myReservationDateStartTime.before(listReservationDateStartTime) || myReservationDateStartTime.compareTo(listReservationDateStartTime) == 0) &&
+                            ((myReservationDateEndTime.before(listReservationDateEndTime) || myReservationDateEndTime.compareTo(listReservationDateEndTime) == 0) &&
+                            myReservationDateEndTime.after(listReservationDateStartTime))) {
+                        saveReservation = false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (saveReservation) {
+            saveReserve(reserve);
+        } else {
+            Toast.makeText(getActivity(), "There already is a reservation at this date/time!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void saveReserve(Map <String, Object> reserve) {
+        db.collection("reservation")
+                .add(reserve)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getActivity(), "Saved reserve!", Toast.LENGTH_SHORT).show();
+                    cleanFields();
+                })
+                .addOnFailureListener(e -> Log.w("failure", "Error adding document", e));
     }
 
     public void populaSpinner() {
